c875d24ff72177be0f670aedade95473d3567356
hochanfs <hochan.lee@financescript.com>
2019-07-28 00:25:03 -0400

    added MockMvc Tests for Owner Controller. Closes #52

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,71 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController controller;
+
+    private Set<Owner> owners;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", Matchers.hasSize(2)));
+    }
+
+    @Test
+    void listOwnersByIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/index"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", Matchers.hasSize(2)));
+    }
+
+    @Test
+    void findOwners() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/find"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("notImplemented"));
+        verifyZeroInteractions(ownerService);
+    }
+}
